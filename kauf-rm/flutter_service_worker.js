299e50a851335ea6c86cd7bbb98c1863adc6f6fe
Marc Unkelbach <remarc@Marcs-MBP.fritz.box>
2022-05-08 18:33:13 +0200

    Update kauf-rm styles

--- kauf-rm/flutter_service_worker.js
+++ kauf-rm/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "cd877b2d84c1484526ffd910f5173bbf",
-"index.html": "0aabdbf79344597198bf0af12e636298",
-"/": "0aabdbf79344597198bf0af12e636298",
-"main.dart.js": "cce01a2f2fea870ff91b21ebd10c0301",
+"index.html": "c9ec390fd4c323076f75ec79822e3899",
+"/": "c9ec390fd4c323076f75ec79822e3899",
+"main.dart.js": "fda5aa8fc8173c7f1ecbe51c7d8fc80f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "8468b2bd854b1a5d0ebb7c5187179012",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "5e7feb15b4ea57eab8ff9bc6da0322de",
+"assets/NOTICES": "381b12cfc15edf5fddd49c3e205773fd",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
