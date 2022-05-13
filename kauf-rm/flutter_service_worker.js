9e5d67fb7d548fdd2e5e62ed13f6d2c54ba56295
munkelb1 <marc.unkelbach@smail.th-koeln.de>
2022-05-13 19:45:53 +0200

    Update kauf-rm

--- kauf-rm/flutter_service_worker.js
+++ kauf-rm/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "cd877b2d84c1484526ffd910f5173bbf",
-"index.html": "b7b6eaf2de4f66b58c6a9e637bf89e88",
-"/": "b7b6eaf2de4f66b58c6a9e637bf89e88",
-"main.dart.js": "73610f4efc9c8c11851bbfe56fbe13d2",
+"index.html": "307cd61b72adb9e17c53ce2c12b06111",
+"/": "307cd61b72adb9e17c53ce2c12b06111",
+"main.dart.js": "ed5af8d00ad0cf4e1110c4a5be3dd8a8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
